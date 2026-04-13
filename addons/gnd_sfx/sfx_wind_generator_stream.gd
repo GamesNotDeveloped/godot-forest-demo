0b500989d4a7b898403a9ee88e346144ae1b03b3
Marcin Nowak <marcin.j.nowak@gmail.com>
2026-04-13 03:37:28 +0200

    wind sound generate (fixes)

--- addons/gnd_sfx/sfx_wind_generator_stream.gd
+++ addons/gnd_sfx/sfx_wind_generator_stream.gd
@@ -7,7 +7,9 @@ class RuntimeState:
 	var dark_lp := 0.0
 	var bright_lp := 0.0
 	var side_lp := 0.0
-	var turbulence_lp := 0.0
+	var gust_lp := 0.0
+	var flutter_lp := 0.0
+	var shimmer_lp := 0.0
 
 
 const TAU := PI * 2.0
@@ -58,24 +60,40 @@ func fill_buffer(runtime: RuntimeState, speed: float) -> void:
 	var master_gain := _sample_speed_curve(master_gain_curve, clamped_speed, 1.0)
 	var speed_blend := clampf(clamped_speed / 12.0, 0.0, 1.0)
 
-	var dark_alpha := _cutoff_to_alpha(lerpf(110.0, 420.0, speed_blend), sample_rate)
-	var bright_alpha := _cutoff_to_alpha(lerpf(320.0, 1800.0, speed_blend), sample_rate)
-	var side_alpha := _cutoff_to_alpha(lerpf(90.0, 860.0, speed_blend), sample_rate)
-	var turbulence_alpha := _cutoff_to_alpha(lerpf(0.8, 3.4, speed_blend), sample_rate)
+	var gust_alpha := _cutoff_to_alpha(lerpf(0.06, 0.34, speed_blend), sample_rate)
+	var flutter_alpha := _cutoff_to_alpha(lerpf(0.35, 1.45, speed_blend), sample_rate)
+	var shimmer_alpha := _cutoff_to_alpha(lerpf(1.6, 4.8, speed_blend), sample_rate)
+	var flutter_depth := lerpf(0.025, 0.11, speed_blend)
+	var gust_depth := lerpf(0.18, 0.6, speed_blend)
 
 	for _frame in range(frames_available):
 		var white := runtime.rng.randf_range(-1.0, 1.0)
+		runtime.gust_lp += gust_alpha * (runtime.rng.randf_range(-1.0, 1.0) - runtime.gust_lp)
+		runtime.flutter_lp += flutter_alpha * (runtime.rng.randf_range(-1.0, 1.0) - runtime.flutter_lp)
+		runtime.shimmer_lp += shimmer_alpha * (runtime.rng.randf_range(-1.0, 1.0) - runtime.shimmer_lp)
+
+		var flutter := runtime.flutter_lp * flutter_depth
+		var shimmer := runtime.shimmer_lp * flutter_depth * 0.35
+		var gust_norm := clampf(0.5 + runtime.gust_lp * 0.5 + flutter + shimmer, 0.0, 1.0)
+		var gust_amp := lerpf(1.0 - gust_depth, 1.0, gust_norm)
+		var filter_push := lerpf(0.82, 1.28, gust_norm)
+
+		var dark_alpha := _cutoff_to_alpha(lerpf(95.0, 360.0, speed_blend) * filter_push, sample_rate)
+		var bright_alpha := _cutoff_to_alpha(lerpf(280.0, 1750.0, speed_blend) * filter_push, sample_rate)
+		var side_alpha := _cutoff_to_alpha(lerpf(120.0, 920.0, speed_blend) * filter_push, sample_rate)
+
 		runtime.dark_lp += dark_alpha * (white - runtime.dark_lp)
 		runtime.bright_lp += bright_alpha * (white - runtime.bright_lp)
 		runtime.side_lp += side_alpha * (white - runtime.side_lp)
-		runtime.turbulence_lp += turbulence_alpha * (white - runtime.turbulence_lp)
 
 		var dark := runtime.dark_lp
 		var bright := white - runtime.bright_lp
-		var mono := (dark * dark_gain + bright * bright_gain) * master_gain
-		var turbulence := lerpf(1.0 - turbulence_amount, 1.0, (runtime.turbulence_lp + 1.0) * 0.5)
-		var side := (white - runtime.side_lp) * stereo_width * OUTPUT_HEADROOM * 0.7
-		var output := mono * turbulence * OUTPUT_HEADROOM
+		var body := dark * dark_gain * lerpf(0.8, 1.08, gust_norm)
+		var air := bright * bright_gain * lerpf(0.52, 1.35, gust_norm)
+		var motion := lerpf(1.0 - turbulence_amount, 1.0, gust_norm)
+		var mono := (body + air) * master_gain * gust_amp * motion
+		var side := (white - runtime.side_lp) * stereo_width * OUTPUT_HEADROOM * lerpf(0.22, 0.7, gust_norm)
+		var output := mono * OUTPUT_HEADROOM
 
 		runtime.playback.push_frame(Vector2(
 			clampf(output + side, -1.0, 1.0),
